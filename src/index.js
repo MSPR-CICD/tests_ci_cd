69d63acdd53577f9c15b7fbabae940766d637c4c
Thomas Le Floch <t.lefloch53gmail.com>
2020-02-10 14:24:11 +0100

    feat: added hello route
    it says hello to the name in query arg
    eg: /hello?name=michel  returns { message: 'Hello michel' }

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const Hapi = require('@hapi/hapi');
+const registerHelloRoutes = require('./hello/hello.routes')
 
 const server = Hapi.server({
   host: 'localhost',
   port: 3000
 });
+
+registerHelloRoutes(server)
 
 server.start().then(() => console.log(`Server running on ${server.info.uri}`))
