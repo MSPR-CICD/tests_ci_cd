9d31f14d766ffef56525f788338eaf31a9bedee0
Thomas Le Floch <t.lefloch53gmail.com>
2020-02-10 15:30:25 +0100

    docs: added swagger specification

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 const Hapi = require('@hapi/hapi');
-const registerHelloRoutes = require('./hello/hello.routes')
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('../package');
+const registerHelloRoutes = require('./hello/hello.routes');
 
 const server = Hapi.server({
   host: 'localhost',
   port: 3000
 });
 
-registerHelloRoutes(server)
 
-server.start().then(() => console.log(`Server running on ${server.info.uri}`))
+const swaggerOptions = {
+  info: {
+    title: 'Test API Documentation',
+    version: Pack.version,
+  },
+};
+
+server.register([
+  Inert,
+  Vision,
+  {
+    plugin: HapiSwagger,
+    options: swaggerOptions
+  }
+]).then(() => {
+  registerHelloRoutes(server)
+}).then(() => {
+  server.start()
+}).then(() => console.log(`Server running on ${server.info.uri}`))
+
