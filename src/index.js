bc59b4a1a4d9e9e2f988d161063a2d9b770f2282
Th30phaN3 <theophane.pro@tuta.io>
2020-02-13 15:32:33 +0100

    speling is difikult

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const package = require("../package");
 const registerHelloRoutes = require("./hello/hello.routes");
 
 const server = Hapi.server({
-  host: "localhost",
   port: process.env.PORT || 3000
 });
 
