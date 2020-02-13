bf5f62f556da955868d285279b450d18cb8def07
Th30phaN3 <theophane.pro@tuta.io>
2020-02-13 15:14:25 +0100

    Something fixed

--- src/index.js
+++ src/index.js
@@ -1,33 +1,35 @@
-const Hapi = require('@hapi/hapi');
-const Inert = require('@hapi/inert');
-const Vision = require('@hapi/vision');
-const HapiSwagger = require('hapi-swagger');
-const package = require('../package');
-const registerHelloRoutes = require('./hello/hello.routes');
+const Hapi = require("@hapi/hapi");
+const Inert = require("@hapi/inert");
+const Vision = require("@hapi/vision");
+const HapiSwagger = require("hapi-swagger");
+const package = require("../package");
+const registerHelloRoutes = require("./hello/hello.routes");
 
 const server = Hapi.server({
-  host: 'localhost',
-  port: 3000
+  host: "localhost",
+  port: process.env.PORT || 3000
 });
-
 
 const swaggerOptions = {
   info: {
-    title: 'Test API Documentation',
-    version: package.version,
-  },
+    title: "Test API Documentation",
+    version: package.version
+  }
 };
 
-server.register([
-  Inert,
-  Vision,
-  {
-    plugin: HapiSwagger,
-    options: swaggerOptions
-  }
-]).then(() => {
-  registerHelloRoutes(server)
-}).then(() => {
-  server.start()
-}).then(() => console.log(`Server running on ${server.info.uri}`))
-
+server
+  .register([
+    Inert,
+    Vision,
+    {
+      plugin: HapiSwagger,
+      options: swaggerOptions
+    }
+  ])
+  .then(() => {
+    registerHelloRoutes(server);
+  })
+  .then(() => {
+    server.start();
+  })
+  .then(() => console.log(`Server running on ${server.info.uri}`));
