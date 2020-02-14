defa8cc7505c61148121cf61eba36af1ac513621
Th30phaN3 <theophane.pro@tuta.io>
2020-02-14 15:45:37 +0100

    Still can't get this right...

--- src/hello/hello.routes.js
+++ src/hello/hello.routes.js
@@ -1,22 +1,22 @@
-const Joi = require('@hapi/joi')
+const Joi = require("@hapi/joi");
 
-const registerHelloRoutes = (server) => {
+const registerHelloRoutes = server => {
   server.route({
-    method: 'GET',
-    path: '/hello',
+    method: "GET",
+    path: "/hello",
     options: {
-      description: 'Says hello to the given name',
-      tags: ['api'],
+      description: "Says hello to the given name",
+      tags: ["api"],
       validate: {
         query: Joi.object({
           name: Joi.string().required()
         })
       }
     },
-    handler: (req, h) => {
-      return { message: `Hello ${req.query.name}`}
-    }, 
-  })
-}
+    handler: req => {
+      return { message: `Hello ${req.query.name}` };
+    }
+  });
+};
 
-module.exports = registerHelloRoutes
+module.exports = registerHelloRoutes;
